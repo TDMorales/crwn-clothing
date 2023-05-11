cbd9153e1e353983a2bb25b5b0ad3e2d52420daf
Timothy Morales <timothydanielmorales@gmail.com>
2023-05-10 23:44:20 -0500

    added product context and products to shop page

--- src/routes/Shop/shop.component.jsx
+++ src/routes/Shop/shop.component.jsx
@@ -1,13 +1,14 @@
-import React from "react";
-import SHOP_DATA from '../../assets/shop-data.json';
+import { React, useContext } from "react";
+import { ProductContext } from "../../contexts/product.context";
+import ProductCard from "../../components/product-card/product-card.component";
+import './shop.styles.scss'
 
 const Shop = () => {
+  const { products } = useContext(ProductContext)
   return (
-    <div>
-      {SHOP_DATA.map(({ id, name }) => (
-        <div key={id}>
-          <h1>{name} </h1>
-        </div>
+    <div className="products-container">
+      {products.map(product => (
+        <ProductCard key={product.id} product={product} />
       ))}
     </div>
   )
