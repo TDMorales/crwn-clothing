5483b88d499ea19b001f8225c55b66bc73aad9c0
TDMorales <timothydanielmorales@gmail.com>
2023-05-18 13:24:00 -0500

    shop router setup and category pages complete

---
+++ src/routes/Category/categoryPage.component.jsx
@@ -0,0 +1,28 @@
+import { Fragment, React, useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { CategoriesContext } from "../../contexts/categories.context";
+import ProductCard from "../../components/product-card/product-card.component";
+
+import "./categoryPage.styles.scss";
+const CategoryPage = () => {
+  const { category } = useParams();
+  const { categoriesMap } = useContext(CategoriesContext);
+  const [products, setProducts] = useState(categoriesMap[category]);
+
+  useEffect(() => {
+    setProducts(categoriesMap[category]);
+  }, [category, categoriesMap]);
+  return (
+    <Fragment>
+      <h2 className="category-page-title">{category.toUpperCase()}</h2>
+      <div className="category-page-container">
+        {products &&
+          products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+      </div>
+    </Fragment>
+  );
+};
+
+export default CategoryPage;
